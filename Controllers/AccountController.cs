da4784c3e1bfa41d1e46427ae6a690b8c17afb41
Samuel Tijani <omalitijani@gmail.com>
2020-09-21 15:14:46 +0100

    Implement account controller

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Electronics_market_backend.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -19,6 +20,46 @@ namespace Electronics_market_backend.Controllers
         {
             _userManager = userManager;
             _signInManager = signInManager;
+        }
+
+        [HttpPost("action")]
+        public async Task<IActionResult> Register([FromBody] RegistrationViewModel formData)
+        {
+            // Will hold all the errors related to registration
+            List<string> errorList = new List<string>();
+
+            var user = new IdentityUser
+            {
+                Email = formData.Email,
+                UserName = formData.UserName,
+                SecurityStamp = Guid.NewGuid().ToString()
+            };
+
+            var result = await _userManager.CreateAsync(user, formData.Password);
+
+            if (result.Succeeded)
+            {
+                await _userManager.AddToRolesAsync(user, "Customer");
+
+                // Sending Confirmation Email
+
+                return Ok(new {
+                    username = user.UserName,
+                    email = user.Email,
+                    status = 1,
+                    message = "Registration Successful"
+                });
+            }
+            else
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                    errorList.Add(error.Description);
+                }
+            }
+
+            return BadRequest(new JsonResult(errorList));
         }
 
     }
