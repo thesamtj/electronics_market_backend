22243546a3b194ad9b6647e5225ac0a42eac1085
Samuel Tijani <omalitijani@gmail.com>
2020-09-24 13:51:08 +0100

    Set up email

---
+++ Email/SendEmailResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Electronics_market_backend.Email
+{
+    public class SendEmailResponse
+    {
+        public bool Successful => ErrorMsg == null;
+
+        public string ErrorMsg { get; set; }
+    }
+}
---
+++ Services/SendGridEmailSender.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Electronics_market_backend.Services
+{
+    public class SendGridEmailSender
+    {
+    }
+}
